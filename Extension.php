999e78d7b8e13a2ed58a6ff1a2eeeaf3c3f30b8e
Gawain Lynch <gawain.lynch@gmail.com>
2014-10-12 11:49:01 +0100

    Remove unused info()

--- Extension.php
+++ Extension.php
@@ -10,27 +10,6 @@ class Extension extends \Bolt\BaseExtension
 
     private $smallwords = "a,aan,achter,af,an,and,at,bij,binnen,boven,buiten,but,by,de,door,een,else,en,for,from,het,if,in,in,into,inzake,is,langs,maar,met,na,naar,naast,nabij,namens,nor,of,of,off,om,omtrent,on,ondanks,onder,ook,op,or,out,over,over,per,richting,rond,rondom,te,tegen,the,then,tijdens,to,tot,tussen,uit,van,vanaf,vanuit,vanwege,via,volgens,voor,voorbij,wegens,when,with,zonder";
 
-    function info()
-    {
-
-        $data = array(
-            'name' =>"TitleCase filter",
-            'description' => "A extension to add a 'titlecase' filter to your templates.",
-            'author' => "Bob den Otter",
-            'link' => "http://bolt.cm",
-            'version' => "1.0",
-            'required_bolt_version' => "1.2",
-            'highest_bolt_version' => "1.2",
-            'type' => "Twig filter",
-            'first_releasedate' => "2013-09-02",
-            'latest_releasedate' => "2013-09-02",
-            'allow_in_user_content' => true,
-        );
-
-        return $data;
-
-    }
-
     public function getName()
     {
         return "Title Case";
